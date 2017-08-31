6db23cfeb7bd413451ab08afb2bd24c0fd83e4d1
Andrii Pankovetskyi <pankovskiy@kasseler-cms.net>
2017-08-31 09:17:39 +0300

    Add checkbox and radiobox

--- Create_form/script.js
+++ Create_form/script.js
@@ -5,35 +5,88 @@
 
 function addToPage() {
     var elSelect = document.getElementById('exampleFormControlSelect1');
-    //var elText = elSelect.options[elSelect.options.selectedIndex].text;
     console.log(elSelect.value);
     var elFormOutput = document.getElementsByName('formoutput')[0];
     console.log(elFormOutput);
 
-    var p = document.createElement('p');
-    elFormOutput.append(p);
+    var div = document.createElement('div');
+    div.setAttribute("class", "form-inline");
+    elFormOutput.append(div);
 
-    var label =document.createElement('label');
+    var label = document.createElement('label');
     label.setAttribute("for", elSelect.value);
-    label.textContent = "Example "+elSelect.value;
-    p.prepend(label);
-    var ourElement = document.createElement(elSelect.value);
-    ourElement.setAttribute("id", elSelect.value);
-    p.append(ourElement);
+    // label.setAttribute("class", "mr-2");
+    label.textContent = "Example " + elSelect.value;
+    div.prepend(label);
 
+    switch (elSelect.value) {
+        case 'input':
+        case 'textarea':
+
+            var ourElement = document.createElement(elSelect.value);
+            div.append(ourElement);
+            console.log(div);
+            break;
+
+        case 'checkbox':
+        case 'radio':
+            var mainDiv=document.createElement('div');
+            div.append(mainDiv);
+
+            var divForCheckbox = document.createElement('div');
+            divForCheckbox.setAttribute('class', 'form-check');
+            mainDiv.append(divForCheckbox);
+
+            var input = document.createElement('input');
+            input.setAttribute('class', 'form-check-input');
+            input.setAttribute('type', elSelect.value);
+            input.setAttribute('name', 'example'+elSelect.value);
+            input.setAttribute('value', 'option1');
+            // inputOne.textContent = "First";
+            divForCheckbox.append(input);
+            divForCheckbox.append(input.value);
+
+            divForCheckbox = document.createElement('div');
+            divForCheckbox.setAttribute('class', 'form-check');
+            mainDiv.append(divForCheckbox);
+
+
+            var input = document.createElement('input');
+            input.setAttribute('class', 'form-check-input');
+            input.setAttribute('type', elSelect.value);
+            input.setAttribute('name', 'example'+elSelect.value);
+            input.setAttribute('value', 'option2');
+            // inputTwo.textContent = " Second";
+            divForCheckbox.append(input);
+            divForCheckbox.append(input.value);
+            break;
+    }
     var buttonRemove = document.createElement('button');
     buttonRemove.setAttribute("type", "button");
-    buttonRemove.setAttribute("class", "btn btn-dark btn-sm");
-    buttonRemove.textContent = "Remove from page";
-    buttonRemove.onclick = removeFromPage.bind(null,ourElement);
-    p.append(buttonRemove);
+    buttonRemove.setAttribute("class", "btn btn-dark");
+    buttonRemove.textContent = "Remove";
+    buttonRemove.onclick = removeFromPage.bind(null, div);
+    div.append(buttonRemove);
+
+    if (!(document.body.firstElementChild.querySelector("button[type=submit]"))) {
+        console.log(document.body.firstElementChild.lastElementChild);
+        var buttonSubmit = document.createElement('button');
+        buttonSubmit.setAttribute("type", "submit");
+        buttonSubmit.setAttribute("class", "btn btn-primary float-right");
+        buttonSubmit.textContent = "Submit";
+        document.body.firstElementChild.append(buttonSubmit);
+    }
+
 
 }
 
+if (document.getElementsByName('formoutput').length > 1 ) {
+    document.body.firstElementChild.querySelector("button[type=submit]").remove();
+};
+
 function removeFromPage(el) {
     console.log(el);
-    el.parentNode.remove();
-    // this.parentNode.parentNode.removeChild(this.parentNode);
+    el.remove();
 }
 
 document.getElementById('buttonAdd').onclick = addToPage;
