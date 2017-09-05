c564973b126aa5a049273b64e9d74e05af024a9e
Vadym Nemchenko <westa.nvv@gmail.com>
2017-09-05 17:08:57 +0300

    Updated JS script

--- Create_form/script.js
+++ Create_form/script.js
@@ -1,153 +1,118 @@
-// button.onclick = function () {
-//     console.log(exampleFormControlSelect1.options[exampleFormControlSelect1.options.selectedIndex].text);
-// }
-
-
+var index = 0;
 function addToPage() {
-    var elSelect = document.getElementById('exampleFormControlSelect1');
-    console.log(elSelect.value);
-    var elFormOutput = document.getElementsByName('formoutput')[0];
-    console.log(elFormOutput);
-
-    var div = document.createElement('div');
-    div.setAttribute("class", "form-inline");
-    elFormOutput.append(div);
-
-    var label = document.createElement('label');
-    label.setAttribute("for", elSelect.value);
-    // label.setAttribute("class", "mr-2");
-    label.textContent = "Example " + elSelect.value;
-    div.prepend(label);
-
-    switch (elSelect.value) {
+    var container = document.getElementById('formoutput'),
+        controlName = '',
+        controlHTML = '';
+    switch (document.getElementById('exampleFormControlSelect1').value) {
         case 'input':
+            controlName = 'input';
+            controlHTML = '<input class="element">';
+            break;
         case 'textarea':
-
-            var ourElement = document.createElement(elSelect.value);
-            div.append(ourElement);
-            ourElement.classList.add('element');
-            console.log(div);
+            controlName = 'textarea';
+            controlHTML = '<textarea class="element"></textarea>';
             break;
-
         case 'checkbox':
-        case 'radio':
-            var mainDiv=document.createElement('div');
-            div.append(mainDiv);
-
-            var divForCheckbox = document.createElement('div');
-            divForCheckbox.setAttribute('class', 'form-check');
-            mainDiv.append(divForCheckbox);
-
-            var input = document.createElement('input');
-            input.setAttribute('class', 'form-check-input');
-            input.classList.add('element');
-            input.setAttribute('type', elSelect.value);
-            input.setAttribute('name', 'example'+elSelect.value);
-            input.setAttribute('value', 'option1');
-            // inputOne.textContent = "First";
-            divForCheckbox.append(input);
-            divForCheckbox.append(input.value);
-
-            divForCheckbox = document.createElement('div');
-            divForCheckbox.setAttribute('class', 'form-check');
-            mainDiv.append(divForCheckbox);
-
-
-            var input = document.createElement('input');
-            input.setAttribute('class', 'form-check-input');
-            input.classList.add('element');
-            input.setAttribute('type', elSelect.value);
-            input.setAttribute('name', 'example'+elSelect.value);
-            input.setAttribute('value', 'option2');
-            // inputTwo.textContent = " Second";
-            divForCheckbox.append(input);
-            divForCheckbox.append(input.value);
+            controlName = 'checkbox';
+            controlHTML =
+                '<div>\
+                    <div class="form-check">\
+                        <input class="form-check-input element" type="checkbox" name="examplecheckbox" value="option1" />option1\
+                    </div>\
+                    <div class="form-check">\
+                        <input class="form-check-input element" type="checkbox" name="examplecheckbox" value="option2">option2\
+                    </div>\
+                </div>';
             break;
-    }
-    var buttonRemove = document.createElement('button');
-    buttonRemove.setAttribute("type", "button");
-    buttonRemove.setAttribute("class", "btn btn-dark");
-    buttonRemove.classList.add('buttonRemove');
-    buttonRemove.textContent = "Remove";
-    buttonRemove.onclick = removeFromPage.bind(null, div);
-    div.append(buttonRemove);
+        case 'radio':
+            controlName = 'radio';
+            controlHTML =
+                '<div>\
+                    <div class="form-check">\
+                        <input class="form-check-input element" type="radio" name="exampleradio" value="option1" />option1\
+                    </div>\
+                    <div class="form-check">\
+                        <input class="form-check-input element" type="radio" name="exampleradio" value="option2">option2\
+                    </div>\
+                </div>';
+            break;
 
-    if (!(document.body.firstElementChild.querySelector("button[type=submit]"))) {
-        console.log("Testcreate buttonSubmit");
-        var div = document.createElement('div');
-        div.setAttribute("class", "row divSubmit");
-        document.getElementsByClassName('inputblock')[0].after(div);
-        var buttonSubmit = document.createElement('button');
-        buttonSubmit.setAttribute("id", "buttonSubmit");
-        buttonSubmit.setAttribute("type", "submit");
-        buttonSubmit.setAttribute("class", "btn btn-primary");
-        buttonSubmit.textContent = "Submit";
-        div.append(buttonSubmit);
+        default:
+            return false;
+    }
+
+    container.insertAdjacentHTML('beforeend',
+        '<div class="form-inline added-form-block">\
+                <label for="input">Example ' + index + ' ' + controlName + '</label>\
+            ' + controlHTML + '\
+            <button type="button" class="btn btn-dark buttonRemove">Remove</button>\
+        </div>'
+    );
+    index++;
+
+    bindRemoveEvent();
+
+    if (!document.getElementById('buttonSubmit')) {
+        document.querySelector('.container').insertAdjacentHTML('beforeend',
+            '<div class="row divSubmit">\
+                <button id="buttonSubmit" type="submit" class="btn btn-primary">Submit</button>\
+            </div>\
+            <div class="result-block"></div>'
+        );
+
         document.getElementById('buttonSubmit').onclick = submitToPage;
     }
-
-
 }
 
-function removeFromPage(el) {
-    console.log(el);
-    el.remove();
-    if (!(document.body.getElementsByClassName("buttonRemove")[0])) {
-        document.getElementsByClassName('divSubmit')[0].remove();
-    };
+function removeFromPage(index) {
+    document.querySelector(".added-form-block:nth-child(" + index + ")").remove();
+
+    bindRemoveEvent();
+    if (document.getElementById('formoutput').childElementCount === 0) {
+        document.querySelector('.divSubmit').remove();
+    }
+}
+
+function bindRemoveEvent() {
+    document.querySelectorAll(".buttonRemove").forEach(function(obj, index) {
+        obj.onclick = removeFromPage.bind(this, index + 1);
+    });
 }
 
 function submitToPage() {
-    if (!(document.getElementsByClassName('outblock')[0])) {
-        var div = document.createElement('div');
-        div.setAttribute("class", "row outblock");
-        document.getElementsByClassName('divSubmit')[0].after(div);
-        // div = document.getElementsByClassName('outblock')[0];
-        var el = document.getElementsByClassName("element");
-        for (var i = 0; i < el.length; i++) {
-            var p = document.createElement('p');
-            div.append(p);
-            if (el[i].type == 'radio' || el[i].type == 'checkbox') {
-                p.innerHTML = '<b>' + el[i].tagName + '</b>' + '--------------->' + el[i].value + '--------------->' + el[i].checked;
-            }
-            else {
-                p.innerHTML = '<b>' + el[i].tagName + '</b>' + '--------------->' + el[i].value;
-                console.log(el[i].value);
-            }
+    var outHTML = '';
+    document.querySelectorAll(".added-form-block .element").forEach(function(obj) {
+        var value = '';
+        switch (obj.tagName.toLowerCase()) {
+            case 'input':
+                value = obj.value;
+                if (obj.type == 'radio' || obj.type == 'checkbox') {
+                    value += '--------------->' + obj.checked;
+                }
+
+                break;
+            case 'textarea':
+                value = obj.value;
+                break;
+            default:
         }
-    }
-    else {
-        div = document.getElementsByClassName('outblock')[0];
-        var el = document.getElementsByClassName("element");
-        for (var i = 0; i < el.length; i++) {
-            var p = document.createElement('p');
-            div.append(p);
-            if (el[i].type == 'radio' || el[i].type == 'checkbox') {
-                p.innerHTML = '<b>' + el[i].tagName + '</b>' + '--------------->' + el[i].value + '--------------->' + el[i].checked;
-            }
-            else {
-                p.innerHTML = '<b>' + el[i].tagName + '</b>' + '--------------->' + el[i].value;
-                console.log(el[i].value);
-            }
-        }
-    }
-    if (!(document.getElementById('buttonClear'))) {
-        var div = document.createElement('div');
-        div.setAttribute("class", "row divClear");
-        document.getElementsByClassName('outblock')[0].after(div);
-        var buttonClear = document.createElement('button');
-        buttonClear.setAttribute("id", "buttonClear");
-        buttonClear.setAttribute("class", "btn btn-secondary");
-        buttonClear.textContent = "Clear";
-        div.append(buttonClear);
+
+        outHTML += '<b>' + obj.tagName.toLowerCase() + '</b>' + value + '<br/>';
+        console.log(obj.tagName.toLowerCase(), value);
+    });
+    if (outHTML !== '') {
+        document.querySelector('.result-block').innerHTML = outHTML +
+            '<div class="row divClear">\
+                <button id="buttonClear" class="btn btn-secondary">Clear</button>\
+            </div>';
+
         document.getElementById('buttonClear').onclick = clearPage;
     }
 
 }
 
 function clearPage() {
-    document.getElementsByClassName('divClear')[0].remove();
-    document.getElementsByClassName('outblock')[0].remove();
+    document.querySelector('.result-block').innerHTML = '';
 }
 
 document.getElementById('buttonAdd').onclick = addToPage;
